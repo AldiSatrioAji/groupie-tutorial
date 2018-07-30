9bf47cc5867fc3161e51cb9ba43d4d88ec675efa
Aldi Satrio Aji <aldis85@yahoo.com>
2018-07-30 21:13:39 +0700

    fix bugs

--- app/src/main/java/com/example/chopper/groupie/HeroAdapter.kt
+++ app/src/main/java/com/example/chopper/groupie/HeroAdapter.kt
@@ -1,8 +1,8 @@
 package com.example.chopper.groupie
 
 import android.content.Context
-import com.bumptech.glide.Glide
 import com.example.chopper.groupie.model.Hero
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.list_item.view.*
@@ -15,7 +15,7 @@ class HeroAdapter(val hero: Hero) : Item<ViewHolder>() {
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.tvHero.text = hero.name
-        Glide.with(this!!.context!!)
+        Picasso.get()
                 .load(hero.image)
                 .into(viewHolder.itemView.ivHero)
     }
