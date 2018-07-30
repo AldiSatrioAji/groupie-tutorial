651dfdb927ecf4703978bd63c2626913851b79c7
Aldi Satrio Aji <aldis85@yahoo.com>
2018-07-30 20:53:38 +0700

    added internet permission

--- app/src/main/java/com/example/chopper/groupie/MainActivity.kt
+++ app/src/main/java/com/example/chopper/groupie/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     private var groupAdapter = GroupAdapter<ViewHolder>()
     private lateinit var heroAdapter: HeroAdapter
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
